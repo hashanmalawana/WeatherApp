16d907a74cd35956b008076ff5c223e0f870f17a
hashanmalawana <hashanmalawana@gmail.com>
2018-09-20 21:40:17 +0530

    Finalized 1.1

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,6 @@ import { SettingsPage } from './../settings/settings';
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
 @Component({
